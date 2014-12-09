cffa79b291775495c6e38f0ebcecf0ec37e8cef2
Joshua Gomez <joshuago78@gmail.com>
2014-12-09 10:27:13 -0800

    Refactored records module to use inheritance from an abstract record to Bib. This can be used for Holdings and Item records as well.

--- pyalma/records.py
+++ pyalma/records.py
@@ -3,30 +3,28 @@ from io import StringIO
 import pymarc
 
 
-class Bib(object):
+class Record(object):
 
-	def __init__(self, data=None, api=None, mms_id=None):
-		self._api = api
-		if data is not None:
-			self.load(data)
-		elif mms_id is not None:
-			self.mms_id = mms_id
-			self.load()
+	def __init__(self, rectype, data={}):
+		self._rectype = rectype
+		self.load(data)
 
-	def load(self, data=None):
-		if data is None:
-			res = self._api.request('GET', 'bib', ids={'mms_id':self.mms_id})
-			data = res.json()
+	def load(self, data):
+		self.data = {}
 		for field, value in data.items():
 			if field == 'anies':
 				self.parse_marc(value[0])
-			elif field == 'holdings':
-				self.holdingslink = value['link']
 			else:
-				setattr(self, field, value)
+				self.data[field] = value
 
 	def parse_marc(self, marcxml):
 		handler = pymarc.XmlHandler()
 		stream = StringIO(marcxml)
 		pymarc.parse_xml(stream, handler)
 		self.marc = handler.records[0]
+
+
+class Bib(Record):
+
+	def __init__(self, data={}):
+		super().__init__('bib', data)
--- test/test_records.py
+++ test/test_records.py
@@ -16,8 +16,8 @@ class TestBibRecord(unittest.TestCase):
             self.bibdata = json.loads(f.read())
 
     def assertBibEqual(self, bib):
-        self.assertEqual(bib.mms_id, 9922405930001552)
-        self.assertEqual(bib.created_date, '2013-07-14Z')
+        self.assertEqual(bib.data['mms_id'], 9922405930001552)
+        self.assertEqual(bib.data['created_date'], '2013-07-14Z')
 
     def test_bib_load(self):
         bib = records.Bib()
@@ -32,17 +32,3 @@ class TestBibRecord(unittest.TestCase):
         bib = records.Bib(self.bibdata)
         self.assertIsInstance(bib.marc, pymarc.record.Record)
         self.assertEqual(bib.marc.author(), 'Tufte, Edward R., 1942-')
-
-    @responses.activate
-    def test_alt_init(self):
-        os.environ['ALMA_API_KEY'] = 'my fake key'
-        os.environ['ALMA_API_REGION'] = 'APAC'
-        reload(alma)
-        biburl = 'https://api-ap.hosted.exlibrisgroup.com/almaws/v1/bibs/9922405930001552'
-        responses.add(responses.GET, biburl, 
-            status=200, 
-            content_type='application/json',
-            body=json.dumps(self.bibdata))
-        api = alma.Alma()
-        bib = records.Bib(api=api, mms_id=9922405930001552)
-        self.assertBibEqual(bib)
