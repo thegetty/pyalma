5431106ea0b544e525cd26e256dc2c51e62b41d4
Joshua Gomez <JGomez@getty.edu>
2015-04-21 13:27:58 -0700

    added setup file for pip installations

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyalma',
+    packages = ['pymarc', 'requests', 'responses'],
+    version = '0.0.1',
+    description = 'Python client for ExLibris Alma',
+    author = 'Getty Research Institute',
+    author_email = 'jgomez@getty.edu',
+    url = 'https://stash.getty.edu/projects/GRIIS/repos/pyalma/browse',
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: pre-Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
